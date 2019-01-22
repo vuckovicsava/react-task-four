a793655b1746c074f6cd285f48f96cac54a82159
Sava <sava.vuckovic@quantox.com>
2019-01-22 12:18:25 +0100

    added a spinner

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { ClipLoader } from 'react-spinners';
 import axios from 'axios';
 
 class App extends Component {
@@ -6,41 +7,42 @@ class App extends Component {
   state = {
     lat: '',
     lng: '',
-    error: false,
     flights: [],
+    error: false,
     fetching: false
   }
 
   getGeolocationData = () => {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(position => {
-        // this.setState({
-        //   lat: position.coords.latitude,
-        //   lng: position.coords.longitude
-        // })
-        this.makeAPICall(position.coords.latitude, position.coords.longitude);
-      }, err => {
-        console.log(err);
-        this.setState({ error: 'Geolocation has to be supported ' })
-      });
+        // set lat & lng once the user allows access to geolocation and call the API
+        this.setState(
+          {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+          }, 
+          () => this.getFlights()
+        );
+      }, err => this.setState({ error: 'Geolocation has to be enabled in order to get flights data ' }));
     } else {
       this.setState({ error: 'Geolocation is not supported by your browser.' });
     }
   }
 
-  makeAPICall = (lat, lng) => {
-    const URL = `https://cors-anywhere.herokuapp.com/https://public-api.adsbexchange.com/VirtualRadar/AircraftList.json?lat=${lat}&lng=${lng}&fDstL=0&fDstU=1000`;
+  getFlights = () => {
+    const URL = `https://cors-anywhere.herokuapp.com/https://public-api.adsbexchange.com/VirtualRadar/AircraftList.json?lat=${this.state.lat}&lng=${this.state.lng}&fDstL=0&fDstU=1000`;
     this.setState({ fetching: true });
     axios.get(URL)
       .then(res => res.data)
       .then(data => {
-        console.log(data);
+        console.log(data); // inspect the data | REMOVE LATER
+        // put the flight data to state and end fetching
         this.setState({
           flights: data.acList,
           fetching: false
-        })
+        });
       })
-      .catch(error => this.setState({ error }))
+      .catch(() => this.setState({ error: 'An error occured while fetching data' }));
   }
 
   renderFlights = () => {
@@ -60,13 +62,19 @@ class App extends Component {
       <div className="App">
         <h1>React Task Four</h1>
         <p>{ this.state.error }</p>
-
         <button onClick={this.getGeolocationData}>Get Flights</button>
-        <h1>{null}</h1>
 
         <hr/>
-        {
-          this.state.fetching ? <div>FETCHING</div> : this.renderFlights() 
+
+        { 
+          this.state.fetching 
+            ? <ClipLoader
+                sizeUnit={"px"}
+                size={150}
+                color={'#123abc'}
+                loading={this.state.fetching}
+              /> 
+            : this.renderFlights() 
         }
       </div>
     );
